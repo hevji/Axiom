8432713a49f75921bd38ce2d8c9e75fec3d7ff70
hevji <gingerwortel.vanoranje@gmail.com>
2026-08-11 11:58:45 +0200

    Update and rename loader.luau to loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+-- tf you looking at nigga
+-- keep it moving no skidding here
+-- dumbass
+
+-- dsc.gg/68 for skidding
+-- [[ why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga, why you looking at my code nigga. ]]
+local fucking_skids_tryna_steal_my_code_nigga = "https://protected.obscuravm.com/dd960d0bca523ae6b99c063d/c557ce2c9668df172825d27c02c3e4deb1c16e546144685e/download"
+local its_protected_nigga, if_you_really_wanna_skid_check_the_dc_link = pcall(function()
+    loadstring(game:HttpGet(fucking_skids_tryna_steal_my_code_nigga))();
+end)
+
+if not its_protected_nigga then
+    warn("Script failed to load: " .. tostring(if_you_really_wanna_skid_check_the_dc_link))
+end
