2f2bb19d07ac2a0ab6e0897c0e3f64820a228b0f
hevji <gingerwortel.vanoranje@gmail.com>
2026-08-30 12:05:57 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,7 @@
--- sudo pacman -S opsec
+-- i spilled lemonade on my mousepad
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
 loadstring(game:HttpGet("https://protected.obscuravm.com/dd960d0bca523ae6b99c063d/c557ce2c9668df172825d27c02c3e4deb1c16e546144685e/download"))();
