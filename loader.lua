315588856478b2bcc00d90e57e1014c63a5f95cd
hevji <gingerwortel.vanoranje@gmail.com>
2026-09-07 19:20:45 +0200

    Change script loading URL in loader.lua
    Updated the URL for loading the script from ObscuraVM.

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
--- i spilled lemonade on my mousepad
+-- hi
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet("https://protected.obscuravm.com/dd960d0bca523ae6b99c063d/c557ce2c9668df172825d27c02c3e4deb1c16e546144685e/download"))();
+loadstring(game:HttpGet("https://api.obscuravm.com/scripts/4020338686459638802"))();
